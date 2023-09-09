f6dbd10151c4d691d0d30bd6afaca42bfd8e1384
dani24sahu <daniramsahu2001@gmail.com>
2023-09-09 13:43:48 +0530

    added a alert banner on error

--- src/component/login/Login.jsx
+++ src/component/login/Login.jsx
@@ -1,21 +1,36 @@
-import { Button, Form, Input, message } from "antd";
+import { Button, Form, Input, message, Alert } from "antd";
 
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+  const [showAlert, setShowAlert] = useState(false); // [state, function that updates the state
+
   const onFinish = (values) => {
     console.log(values); // values is an object with username and password
 
+    // setTimeout(() => {
+    //   message.success("Login successful"); // function that displays a success message
+    //   // message.error("Login failed"); // function that displays an error message
+    //   // message.warning("Login failed"); // function that displays a warning message
+    // }, 1000);
+
     setTimeout(() => {
-      message.success("Login successful"); // function that displays a success message
-      // message.error("Login failed"); // function that displays an error message
-      // message.warning("Login failed"); // function that displays a warning message
+      setShowAlert(true);
     }, 1000);
   };
 
   return (
     <>
       <div className="loginForm">
+        {showAlert && (
+          <Alert
+            type="error"
+            message="Login failed"
+            description="There was an error login your account"
+            banner
+            closable
+          ></Alert>
+        )}
         <Form onFinish={onFinish}>
           <Form.Item label="Username" name="username" required>
             <Input placeholder="Enter username"></Input>
