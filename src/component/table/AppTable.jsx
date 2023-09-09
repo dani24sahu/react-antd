e087eae64b92bf3482231213c2816b81d5df88e0
dani24sahu <daniramsahu2001@gmail.com>
2023-09-09 13:13:47 +0530

    added a sorter key in title and age columns

--- src/component/table/AppTable.jsx
+++ src/component/table/AppTable.jsx
@@ -29,11 +29,13 @@ const AppTable = () => {
       dataIndex: "name",
       key: "key",
       render: (name) => <a>{name}</a>,
+      sorter: (a, b) => a.name.localeCompare(b.name),   // localeCompare() compares two strings according to the current locale. It returns a number that indicates the relative order of the strings. The return value is 0 if the strings are equal, -1 if the first string is sorted before the second, and 1 if the first string is sorted after the second.
     },
     {
       title: "Age",
       dataIndex: "age",
       key: "key",
+      sorter: (a, b) => a.age - b.age, // sort by age ascending order, if a.age - b.age is negative, a is sorted before b, if positive, b is sorted before a, if 0, no change
     },
     {
       title: "Address",
