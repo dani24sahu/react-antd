1225b83155807b5a1586bd06949f92b834d87480
dani24sahu <daniramsahu2001@gmail.com>
2023-09-09 13:29:40 +0530

    added success, error, warning messagea on onFinish event

--- src/component/login/Login.jsx
+++ src/component/login/Login.jsx
@@ -1,12 +1,22 @@
-import { Button, Form, Input } from "antd";
+import { Button, Form, Input, message } from "antd";
 
 import React from "react";
 
 const Login = () => {
+  const onFinish = (values) => {
+    console.log(values); // values is an object with username and password
+
+    setTimeout(() => {
+      message.success("Login successful"); // function that displays a success message
+      // message.error("Login failed"); // function that displays an error message
+      // message.warning("Login failed"); // function that displays a warning message
+    }, 1000);
+  };
+
   return (
     <>
       <div className="loginForm">
-        <Form>
+        <Form onFinish={onFinish}>
           <Form.Item label="Username" name="username" required>
             <Input placeholder="Enter username"></Input>
           </Form.Item>
