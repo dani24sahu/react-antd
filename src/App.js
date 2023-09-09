119dfee1a8eb81de4e10a1c825549a3f7ebe8445
dani24sahu <daniramsahu2001@gmail.com>
2023-09-09 10:10:46 +0530

    Created Contact and about component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import AppContact from "./component/contact/Contact";
+import AppAbout from "./component/about/About";
+
+function App() {
+  return (
+    <>
+      {/* <AppContact /> */}
+      <AppAbout />
+    </>
+  );
+}
+
+export default App;
