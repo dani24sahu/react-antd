1753c1c582f94d1ac9b83e6d6744df39134884aa
dani24sahu <daniramsahu2001@gmail.com>
2023-09-09 13:20:26 +0530

    started using Login component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,14 @@ import "./App.css";
 import AppContact from "./component/contact/Contact";
 import AppAbout from "./component/about/About";
 import Login from "./component/login/Login";
-// import Head from "./component/layout/Header";
 import AppTable from "./component/table/AppTable";
 
 function App() {
   return (
     <>
       {/* <AppContact /> */}
-      {/* <Login /> */}
-      {/* <Head /> */}
-      <AppTable />
+      <Login />
+      {/* <AppTable /> */}
     </>
   );
 }
