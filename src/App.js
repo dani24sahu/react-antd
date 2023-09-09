cc3a8e02f298ad1086eb14037280cd2c2896cde8
dani24sahu <daniramsahu2001@gmail.com>
2023-09-09 12:53:57 +0530

    Imported AppTable component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import "./App.css";
 import AppContact from "./component/contact/Contact";
 import AppAbout from "./component/about/About";
 import Login from "./component/login/Login";
+// import Head from "./component/layout/Header";
+import AppTable from "./component/table/AppTable";
 
 function App() {
   return (
     <>
       {/* <AppContact /> */}
-      <Login />
+      {/* <Login /> */}
+      {/* <Head /> */}
+      <AppTable />
     </>
   );
 }
